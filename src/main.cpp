0d1507addb6b9766c3daef7802c8b4318ffe8eac
Isac Marín Sirias <isacms11@gmail.com>
2022-05-08 00:46:04 -0600

    Creando y  leyendo un BMP

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Image.h"
+#include "Image.cpp"
+
+
+
+int main(){
+    const int width = 640;
+    const int height = 480;
+
+    Image image(width,height);
+
+    for(int y = 0; y < height ; y++)
+    {
+        for(int x = 0; x<width ; x++){
+
+            image.setColor(Color(111,222,111),x , y);
+        }
+    }
+    image.Export("imageprueba.bmp");
+
+    int i = 233;
+
+    for(int m = 0; m < 400; m++ ){
+        image.setColor(Color(255,255,255), m, i);
+    }
+
+
+    image.Read("imageprueba.bmp");
+
+    image.Export("copy.bmp");
+
+    return 0;
+}
