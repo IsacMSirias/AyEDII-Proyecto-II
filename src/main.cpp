b8076d019f209595daec96320a5f3e1395945825
Isac Marín Sirias <isacms11@gmail.com>
2022-05-09 22:12:10 -0600

    Rotacion de imagen a 90 y 180 grados
    Hace falta arreglar la exportacion de la imagen, porque eso se echa todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
-#include "Image.h"
-#include "Image.cpp"
+#include "image.hpp"
 
+int main() {
+	std::cout << "Hello, BMP!" << std::endl;
 
+	Image img;
+    img.generate(400,400);
+	
+    img.Read("in.bmp");
+	img.rotate180();
+	img.Export("out.bmp");
 
-int main(){
-    const int width = 640;
-    const int height = 480;
+	img.clear();
 
-    Image image(width,height);
-
-    for(int y = 0; y < height ; y++)
-    {
-        for(int x = 0; x<width ; x++){
-
-            image.setColor(Color(111,222,111),x , y);
-        }
-    }
-    image.Export("imageprueba.bmp");
-
-    int i = 233;
-
-    for(int m = 0; m < 400; m++ ){
-        image.setColor(Color(255,255,255), m, i);
-    }
-
-
-    image.Read("imageprueba.bmp");
-
-    image.Export("copy.bmp");
-
-    return 0;
-    
-}// ekisde, no se hizo el commid unu 
+	return 0;
+}
